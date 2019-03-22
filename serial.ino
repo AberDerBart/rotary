14035afc8804581227c77e7547f02e981682d8c5
AberDerBart <nonatz@web.de>
2019-03-22 16:11:30 +0100

    implement programming of speed dial numbers

--- serial.ino
+++ serial.ino
@@ -1,5 +1,5 @@
+// for handling serial connects and disconnects
 bool connectionActive(){
-	// for handling serial connects and disconnects
 	static bool active = false;
 
 	if(Serial && !active){
@@ -16,18 +16,45 @@ bool connectionActive(){
 	return active;
 }
 
+void setSpeedDial(){
+	char digit = Serial.read() - '1';
+	
+	// read expected newline
+	Serial.read();
+
+	if(digit >= 0 && digit <= 8){
+		Serial.readBytesUntil('\n',getSpeedDial(digit),29);
+	}else{
+		Serial.println("e");
+	}
+}
+
 bool updateSerial(){
-	//if the state change, write it to serial
 	static void* lastState = NULL;
 
 	if(connectionActive()){
+
+		//if the state change, write it to serial
 		if(lastState != state){
 			Serial.println(stateString(state));
 			lastState = state;
 		}
 
 		if(Serial.available()){
+			// see serial.md for details on the serial communication
+			char function = Serial.read();
 
+			// read expected newline
+			Serial.read();
+
+			switch(function){
+			case 's':
+				setSpeedDial();
+				break;
+			default:
+				Serial.println("e");
+				break;
+			}
 		}
 	}
 }
---
+++ speeddial.ino
@@ -0,0 +1,6 @@
+char speeddial[9][30];
+
+char* getSpeedDial(int digit){
+	return speeddial[digit];
+}
+
